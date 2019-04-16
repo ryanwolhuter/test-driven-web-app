6c862b30add9de663eb68750fc16cf13f50de85e
ry <dev@ryanwolhuter.com>
2019-04-16 17:02:10 +0200

    passing

--- server_test.go
+++ server_test.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"strings"
+	"os"
+	"io/ioutil"
 	"io"
 	"reflect"
 	"encoding/json"
@@ -152,9 +153,10 @@ func TestLeague(t *testing.T) {
 func TestFileSystemStore(t *testing.T) {
 
     t.Run("/league from a reader", func(t *testing.T) {
-        database := strings.NewReader(`[
+        database, cleanDatabase := createTempFile(t, `[
             {"Name": "Cleo", "Wins": 10},
             {"Name": "Chris", "Wins": 33}]`)
+		defer cleanDatabase()
 
         store := FileSystemPlayerStore{database}
 
@@ -172,20 +174,17 @@ func TestFileSystemStore(t *testing.T) {
 	})
 	
 	t.Run("get player score", func(t *testing.T) {
-    database := strings.NewReader(`[
-        {"Name": "Cleo", "Wins": 10},
-        {"Name": "Chris", "Wins": 33}]`)
+        database, cleanDatabase := createTempFile(t, `[
+            {"Name": "Cleo", "Wins": 10},
+            {"Name": "Chris", "Wins": 33}]`)
+        defer cleanDatabase()
 
-    store := FileSystemPlayerStore{database}
+        store := FileSystemPlayerStore{database}
 
-    got := store.GetPlayerScore("Chris")
-
-    want := 33
-
-    if got != want {
-        t.Errorf("got %d want %d", got, want)
-    }
-})
+        got := store.GetPlayerScore("Chris")
+        want := 33
+        assertScoreEquals(t, got, want)
+    })
 }
 
 func assertStatus(t *testing.T, got, want int) {
@@ -200,6 +199,12 @@ func assertResponseBody(t *testing.T, got, want string) {
 	if got != want {
 		t.Errorf("response body is wrong, got '%s', want '%s'", got, want)
 	}
+}
+
+func assertScoreEquals(t *testing.T, got, want int) {
+	if got != want {
+        t.Errorf("got %d want %d", got, want)
+    }
 }
 
 func newPostWinRequest(name string) *http.Request {
@@ -240,4 +245,23 @@ func assertContentType(t *testing.T, response *httptest.ResponseRecorder, want s
     if response.Header().Get("content-type") != want {
         t.Errorf("response did not have content-type of %s, got %v", want, response.HeaderMap)
     }
+}
+
+func createTempFile(t *testing.T, initialData string) (io.ReadWriteSeeker, func()) {
+	t.Helper()
+
+	tmpfile, err := ioutil.TempFile("", "db")
+
+	if err != nil {
+		t.Fatalf("could not create temp file %v", err)
+	}
+
+	tmpfile.Write([]byte(initialData))
+
+	removeFile := func() {
+		tmpfile.Close()
+		os.Remove(tmpfile.Name())
+	}
+
+	return tmpfile, removeFile
 }
